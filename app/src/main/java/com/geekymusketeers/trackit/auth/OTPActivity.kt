f248834107d6dc0ae2ad32c636736076973729d3
Aritra Das <aritrarick2002@gmail.com>
2023-03-14 22:46:24 +0530

    Resend OTP function added ✅

--- app/src/main/java/com/geekymusketeers/trackit/auth/OTPActivity.kt
+++ app/src/main/java/com/geekymusketeers/trackit/auth/OTPActivity.kt
@@ -2,20 +2,24 @@ package com.geekymusketeers.trackit.auth
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.geekymusketeers.trackit.MainActivity
 import com.geekymusketeers.trackit.R
 import com.geekymusketeers.trackit.databinding.ActivityOtpactivityBinding
-import com.geekymusketeers.trackit.databinding.ActivitySignInBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.PhoneAuthCredential
-import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import java.util.concurrent.TimeUnit
+
 
 class OTPActivity : AppCompatActivity() {
 
@@ -45,6 +49,131 @@ class OTPActivity : AppCompatActivity() {
 
         initialization()
         verifyOTP()
+        handleCallBacks()
+        handleEditText()
+
+        binding.resendOtp.setOnClickListener {
+            resendVerificationCode()
+            resendOTPTvVisibility()
+        }
+    }
+
+    private fun handleEditText() {
+        inputOTP1.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    inputOTP2.requestFocus()
+                }
+            }
+        })
+
+        inputOTP2.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    inputOTP3.requestFocus()
+                }
+            }
+        })
+
+        inputOTP3.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    inputOTP4.requestFocus()
+                }
+            }
+        })
+
+        inputOTP4.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    inputOTP5.requestFocus()
+                }
+            }
+        })
+
+        inputOTP5.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    inputOTP6.requestFocus()
+                }
+            }
+        })
+
+        inputOTP6.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable) {}
+        })
+    }
+
+    private fun resendOTPTvVisibility() {
+        inputOTP1.setText("")
+        inputOTP2.setText("")
+        inputOTP3.setText("")
+        inputOTP4.setText("")
+        inputOTP5.setText("")
+        inputOTP6.setText("")
+        binding.apply {
+            resendOtp.visibility = View.GONE
+            resendOtp.isEnabled = false
+        }
+        Handler(Looper.myLooper()!!).postDelayed(Runnable {
+            binding.resendOtp.visibility = View.VISIBLE
+            binding.resendOtp.isEnabled = true
+        }, 30000)
+    }
+
+    private fun resendVerificationCode() {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this)                 // Activity (for callback binding)
+            .setCallbacks(callbacks)
+            .setForceResendingToken(resendToken)// OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+                Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+                Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+            }
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            OTP = verificationId
+            resendToken = token
+        }
+    }
+
+    private fun handleCallBacks() {
+        binding.changeNumber.setOnClickListener {
+            startActivity(Intent(this,SignInActivity::class.java))
+        }
     }
 
     @SuppressLint("CutPasteId")
@@ -81,8 +210,6 @@ class OTPActivity : AppCompatActivity() {
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-
                     Toast.makeText(this, "Authenticate Successfully", Toast.LENGTH_SHORT).show()
                     sendToMain()
                 } else {
@@ -91,7 +218,6 @@ class OTPActivity : AppCompatActivity() {
                     if (task.exception is FirebaseAuthInvalidCredentialsException) {
                         // The verification code entered was invalid
                     }
-                    // Update UI
                 }
             }
     }
