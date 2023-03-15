b070f46cf9abcbdfb3cb932e0b009c31d4d6e883
Aritra Das <aritrarick2002@gmail.com>
2023-03-15 19:54:22 +0530

    Made the User details flow

---
+++ app/src/main/java/com/geekymusketeers/trackit/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.geekymusketeers.trackit.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geekymusketeers.trackit.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
